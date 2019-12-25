4cc74d3529790a88e237c2410949030db4586ed3
DiegoChajtur <godie.tur@gmail.com>
2019-12-25 20:05:55 -0300

    Se agrego prueba de funcionamiento

--- seqcollazt/tests.py
+++ seqcollazt/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.test import Client
+import json
+class ViewTest(TestCase):
 
-# Create your tests here.
+    def testCalc(self):
+        response = Client().get('/seq/?initnum=10')
+        if (response.status_code == 200):
+            print ('Test OK')
--- seqcollazt/views.py
+++ seqcollazt/views.py
@@ -19,16 +19,21 @@ def calc(request):
                     num = (num * 3) + 1
             if num == 1:
                 result += '1'
+            data['result'] = result
+            json_data = json.dumps(data)
         else:
             result = 'El numero debe ser mayor que 0'
-        data['result'] = result
-        json_data = json.dumps(data)
-        return HttpResponse(json_data)
+            data['result'] = result
+            json_data = json.dumps(data)
+            return HttpResponse(json_data, content_type='application/json',status = 400)
+
+        return HttpResponse(json_data, content_type='application/json',status = 200)
+
     except Exception as e:
         result = 'Debe ingresar solo numeros'
         data['result'] = result
         json_data = json.dumps(data)
-        return HttpResponse(json_data)
+        return HttpResponse(json_data, content_type='application/json',status = 400)
 
 def home(request):
     template = loader.get_template('index.html')
